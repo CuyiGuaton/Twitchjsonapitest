db39280f09c6a0e423c539a9232bfebefcae75bd
Sergio Salinas <sergiosalfer@gmail.com>
2016-08-29 21:42:22 -0300

    funciona la llamaday muestra el titulo de la cosa

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-,
+
+$.ajax({
+     url:'https://api.twitch.tv/kraken/streams/ESL_SC2',
+     dataType:'jsonp',
+     success:function(data) {
+       console.log(data.stream.channel.status);
+       $("#result").html(data.stream.channel.status);
+      }
+ });
+
+var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
