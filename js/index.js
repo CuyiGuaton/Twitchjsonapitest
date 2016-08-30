3fa190395047c8def012b2f8125aa2066c1282e0
Sergio Salinas <sergiosalfer@gmail.com>
2016-08-29 23:02:02 -0300

    Funciona las tarjetas, hay que arreglar el foreach que no agrega nada a al link

--- js/index.js
+++ js/index.js
@@ -1,11 +1,30 @@
+function callJSON() {
+//    for (var i in channels) {
+        $.ajax({
+            url: 'https://api.twitch.tv/kraken/channels/ESL_SC2',
+            dataType: 'jsonp',
+            type: 'GET',
+            async: false,
+            headers: {
+                'Api-User-Agent': 'Example/1.0'
+            },
+            success: function(data) {
+                makeCard(data);
+                console.log(data.logo + '\n');
+            }
+        });
+//    }
+}
 
-$.ajax({
-     url:'https://api.twitch.tv/kraken/streams/ESL_SC2',
-     dataType:'jsonp',
-     success:function(data) {
-       console.log(data.stream.channel.status);
-       $("#result").html(data.stream.channel.status);
-      }
- });
+function makeCard(data) {
+    console.log(data.logo + '\n');
+    if (data.logo != undefined) {
+        html = '<h2 class="header">Horizontal Card</h2> <div   class="card horizontal">  <div class="card-image"> <img src="' + data.logo;
+        html += '">      </div>      <div class="card-stacked">        <div class="card-content">          <p>I am a very simple card. I am good at containing small bits of information.</p>        </div>        <div class="card-action">          <a href="#">This is a link</a>        </div>      </div>    </div>'
+        $("#result").html(html);
+    }
+}
 
-var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+
+//var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+callJSON();
