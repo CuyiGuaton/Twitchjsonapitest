c39b3e281ab7dd2f2f1977156e3a8d2a874939cb
Sergio Salinas <sergiosalfer@gmail.com>
2016-08-31 04:32:02 -0300

    pico

--- js/index.js
+++ js/index.js
@@ -1,54 +1,60 @@
 function channelInfo(channelName) {
-    var IsOnline =false, game='', status='', channelLink='', channelLogo='', channelDisplayName='';
-    var data =$.parseJSON( $.ajax({
-        url: 'https://api.twitch.tv/kraken/streams/' + channelName,
-        dataType: 'jsonp',
-        type: 'GET',
-        async: false,
-        success: function(data) {
-            if (data.stream === null) {
-                IsOnline = false;
-                status = "Offline";
-                game = '';
-            } else if (data.stream === undefined) {
-                IsOnline = false;
-                  status = data.message;
-                game = '';
-            } else {
-                IsOnline = true;
-                status = data.stream.channel.status;
-                game = data.stream.game;
-            };
+    var IsOnline = false,
+        game = '',
+        status = '',
+        channelLink = '',
+        channelLogo = '',
+        channelDisplayName = '';
+    var data = $.parseJSON($.ajax({
+            url: 'https://api.twitch.tv/kraken/streams/' + channelName,
+            dataType: 'jsonp',
+            type: 'GET',
+            async: false,
+            success: function(data) {
+                    if (data.stream === null) {
+                        IsOnline = false;
+                        status = "Offline";
+                        game = '';
+                    } else if (data.stream === undefined) {
+                        IsOnline = false;
+                        status = data.message;
+                        game = '';
+                    } else {
+                        IsOnline = true;
+                        status = data.stream.channel.status;
+                        game = data.stream.game;
+                    };
 
-        }
-    }).responseText);
-    $.ajax({
-        url: 'https://api.twitch.tv/kraken/channels/' + channelName,
-        dataType: 'jsonp',
-        type: 'GET',
-        async: false,
-        success: function(data) {
-          channelLogo = data.logo != null ? data.logo : 'http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png';
-          channelDisplayName = data.display_name != null ? data.display_name : channelName;
-          channelLink = data.url != null ? data.url : "https://www.twitch.tv/" + channelName;
-            //  console.log(data.logo + '\n');
-        }
-    });
-    //console.log( game + status + channelLink + channelLogo + channelDisplayName);
-    console.log(game);
-}
 
-function makeCard(data) {
-    console.log(data.logo + '\n');
-    if (data.logo != undefined) {
-        html = '<h2 class="header">Horizontal Card</h2> <div   class="card horizontal">  <div class="card-image"> <img src="' + data.logo;
-        html += '">      </div>      <div class="card-stacked">        <div class="card-content">          <p>I am a very simple card. I am good at containing small bits of information.</p>        </div>        <div class="card-action">          <a href="#">This is a link</a>        </div>      </div>    </div>'
-        $("#result").html(html);
+
+                    $.ajax({
+                        url: 'https://api.twitch.tv/kraken/channels/' + channelName,
+                        dataType: 'jsonp',
+                        type: 'GET',
+                        async: false,
+                        success: function(data) {
+                            channelLogo = data.logo != null ? data.logo : 'http://www.socialgiri.com/wp-content/uploads/2013/08/about-thumbnail-placeholder.png';
+                            channelDisplayName = data.display_name != null ? data.display_name : channelName;
+                            channelLink = data.url != null ? data.url : "https://www.twitch.tv/" + channelName;
+                            //  console.log(data.logo + '\n');
+                        }
+                    });
+                }
+                //console.log( game + status + channelLink + channelLogo + channelDisplayName);
+        });
     }
-}
+
+    function makeCard(data) {
+        console.log(data.logo + '\n');
+        if (data.logo != undefined) {
+            html = '<h2 class="header">Horizontal Card</h2> <div   class="card horizontal">  <div class="card-image"> <img src="' + data.logo;
+            html += '">      </div>      <div class="card-stacked">        <div class="card-content">          <p>I am a very simple card. I am good at containing small bits of information.</p>        </div>        <div class="card-action">          <a href="#">This is a link</a>        </div>      </div>    </div>'
+            $("#result").html(html);
+        }
+    }
 
 
-var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
-for (var i in channels) {
-    channelInfo(channels[i]);
-}
+    var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+    for (var i in channels) {
+        channelInfo(channels[i]);
+    }
